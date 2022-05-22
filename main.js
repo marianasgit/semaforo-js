77257840d3ec12cbd0dcccd0888a1f9e14ce2ccc
Mariana Gomes <marianabicla@gmail.com>
2022-05-22 19:51:30 -0300

    consertando troca de cores

--- main.js
+++ main.js
@@ -1,43 +1,52 @@
-'use strict'
+"use strict";
 
-const semaforo = document.getElementById('semaforo')
-const vermelho = document.getElementById('vermelho')
-const amarelo = document.getElementById('amarelo')
-const verde = document.getElementById('verde')
-const automatico = document.getElementById('automatico')
-let idAutomatico = null;
+const semaforo = document.getElementById("semaforo");
+const vermelho = document.getElementById("vermelho");
+const amarelo = document.getElementById("amarelo");
+const verde = document.getElementById("verde");
+const automatico = document.getElementById("automatico");
 
+let tempoTrocaCores = null;
 
+const luzVermelha = () => (semaforo.src = "./img/vermelho.png");
 
-const luzVermelha = () => semaforo.src = './img/vermelho.png' 
+const luzAmarela = () => (semaforo.src = "./img/amarelo.png");
 
-
-const luzAmarela = () => semaforo.src = './img/amarelo.png' 
-
-
-const luzVerde = () => semaforo.src = './img/verde.png'
-
+const luzVerde = () => (semaforo.src = "./img/verde.png");
 
 const luzAutomatica = () => {
-    if (luzVermelha()) {
-        luzAmarela()
-    } else if (luzVerde()) {
-        luzAmarela()
-    } else {
-        luzVermelha()
-    }
-}
+  let urlLuzAtual = semaforo.src;
+  let luzAtual = urlLuzAtual.split("/")[4];
+
+  if (luzAtual === "desligado.png") {
+    luzVerde();
+  } else if (luzAtual === "verde.png") {
+    luzAmarela();
+  } else if (luzAtual === "amarelo.png") {
+    luzVermelha();
+  } else if (luzAtual === "vermelho.png") {
+    luzVerde();
+  } else {
+    semaforo.src = "./img/desligado.png";
+  }
+};
+
 const trocarCores = () => {
+  tempoTrocaCores = setInterval(luzAutomatica, 500);
+};
 
-    if (idAutomatico == null) {
-        if (!luzVermelha && !luzAmarela && !luzVerde) {
-            idAutomatico = setInterval (luzAutomatica, 500)
-        }
-    }
-}
+vermelho.addEventListener("click", function () {
+  clearInterval(tempoTrocaCores);
+  luzVermelha();
+});
 
+amarelo.addEventListener("click", function () {
+  clearInterval(tempoTrocaCores);
+  luzAmarela();
+});
+verde.addEventListener("click", function () {
+  clearInterval(tempoTrocaCores);
+  luzVerde();
+});
 
-vermelho.addEventListener('click', luzVermelha)
-amarelo.addEventListener('click', luzAmarela)
-verde.addEventListener('click', luzVerde)
-automatico.addEventListener('click', luzAutomatica)
+automatico.addEventListener("click", trocarCores);
